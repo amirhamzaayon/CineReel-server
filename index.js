2845265bc7da6d90e5f0e4174cafc4471f034a68
amirhamzaayon <amirhamzaayon.official@gmail.com>
2025-03-10 23:57:30 +0600

    deploy on vercel as a server side

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://cinereel-optional.web.app",
+      "https://cinereel-optional.firebaseapp.com",
+    ],
     credentials: true,
   })
 );
@@ -36,6 +41,49 @@ async function run() {
     // );
 
     const MoviesInfo = client.db("CineReel").collection("MoviesInfo");
+    const Wishlists = client.db("CineReel").collection("Wishlists");
+
+    // get all movies
+    app.get("/movies", async (req, res) => {
+      const cursor = MoviesInfo.find({});
+      const movies = await cursor.toArray();
+      res.send(movies);
+    });
+
+    // get a single movie
+    app.get("/moviedetails/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const movie = await MoviesInfo.findOne(query);
+      res.send(movie);
+    });
+
+    // add a movie
+    app.post("/add-movie", async (req, res) => {
+      const movie = req.body;
+      const result = await MoviesInfo.insertOne(movie);
+      res.send(result);
+    });
+
+    // add to wishlist
+    app.post("/add-to-wishlist", async (req, res) => {
+      const { userEmail, movieId, movieTitle } = req.body;
+
+      // Update the user's wishlist array by adding the movieId if it's not already present.
+      const result = await Wishlists.updateOne(
+        { userEmail: userEmail },
+        {
+          $addToSet: {
+            userEmail: userEmai,
+            wishlist: movieId,
+            movieTitle: movieTitle,
+          },
+        },
+        { upsert: true }
+      );
+
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -44,7 +92,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("complete initial server setup");
+  res.send("complete initial server setup for CineReel"); //
 });
 
 app.listen(port, () => {
